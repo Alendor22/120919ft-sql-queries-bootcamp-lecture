44a9aa4aed2736b20f69b2a413bb5ea7b21be78b
Enoch2k2 <enoch2k2@hotmail.com>
2020-01-22 13:04:59 -0600

    finished product

---
+++ queries.sql
@@ -0,0 +1,12 @@
+-- We will write our code to copy / paste into the sqlite cli
+
+-- Examples
+
+-- Select only usernames from users
+-- Select only names and levels from characters
+-- Select all attributes from the characters that have a level higher than 50
+-- Select all attributes from users that have characters that have a level higher than 50
+-- Select username and count of characters for every user
+-- Select user username and character count with the highest amount of characters
+-- Select character name and user username with the highest level character
+
